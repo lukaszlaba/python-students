909736836ea067881db2536934bbe547f8c03592
Lukasz <lukaszlaba@gmail.com>
2024-11-10 23:09:55 +0100

    Dodany ciekawy kod

--- names.py
+++ names.py
@@ -1,2 +1,34 @@
-print("Mam na imię")  # tutaj wpisz swoje imię
-# i dodaj jeszcze jakiś ciekawy kod
+print("Mam na imię Łukasz Laba")  # tutaj wpisz swoje imię
+
+# Wyznaczyć 18 list pierwszych za pomocą Python.
+# Przedstawić obrazowo binarny zapis tych liczb w ciekawy sposób.
+
+# wyznaczenie listy liczb pierwszych
+prime_counter = 0
+prime_list = []
+i = 1
+while prime_counter < 18:
+    is_prime = True
+    for j in range(2, i):
+        if i % j == 0:
+            is_prime = False
+            continue
+    if is_prime:
+        prime_list.append(i)
+        prime_counter += 1
+    i += 1
+print('\nLista list pierwszych:')
+print(prime_list)
+
+#tworzenie obrazka
+print('\nPierwszoliczbowa binarna choinka świąteczna z dydykacją dla Prowadzącego :)\n')
+for i in prime_list + [1,1]: # dodatkowe [1,1] to nóżka
+    prime_sting = '{:012b}'.format(i)
+    prime_sting = prime_sting.replace('1', ' X ').replace('0', '   ')
+    print(prime_sting + prime_sting[::-1], '   ', i) #wyswietlennie prime_sting z dołączonym lustrzanym odbiciem
+print(2 * 12 * ' - ')
+
+
+
+
+
